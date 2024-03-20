f1a325008ba6bd205bdf8c9d44d4e75c4218c220
BolinhaXD <rodrigoarraiado@gmail.com>
2024-03-20 10:55:21 +0000

    nivel 1

---
+++ PropertyTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class PropertyTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class PropertyTest
+{
+    /**
+     * Default constructor for test class PropertyTest
+     */
+    public PropertyTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testConstructor(){
+        Property p1 = new Property("T3 Monte Belo", 150000.0);
+        assertEquals("T3 Monte Belo", p1.getDescription());
+        assertEquals(150000.0, p1.getPrice());
+    }
+}
