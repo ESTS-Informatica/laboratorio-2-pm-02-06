07013fe8e44b005ee2ccaa21b9afe6adf93d63d7
BolinhaXD <rodrigoarraiado@gmail.com>
2024-03-20 11:16:38 +0000

    Nivel 3

---
+++ CompanyTest.java
@@ -0,0 +1,51 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class CompanyTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class CompanyTest
+{
+    private Company comp1;
+    
+    /**
+     * Default constructor for test class CompanyTest
+     */
+    public CompanyTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+        comp1 = new Company();
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testConstructor(){
+        assertNotNull(comp1.getProperties());
+        assertNotNull(comp1.getClients());
+        assertNotNull(comp1.getSellers());
+        assertNotNull(comp1.getSells());
+    }
+}
